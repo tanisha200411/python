faa66467bdc8022ab3caad4aa47794acf0271aaa
tanisha200411 <tanishakaranjiya@gmail.com>
2025-04-07 15:44:40 +0530

    02_empty dict

---
+++ learning-python/chapter4/02_problem.py
@@ -0,0 +1,19 @@
+# Create an empty dictionary. Allow 4 friends to enter their favorite language asvalue and use key as their names. Assume that the names are unique. 
+d ={}
+name = input ("enter your name : ")
+lang = input("enter your favourate language : ")
+d.update({name:lang})
+
+name = input ("enter your name : ")
+lang = input("enter your favourate language : ")
+d.update({name:lang})
+
+name = input ("enter your name : ")
+lang = input("enter your favourate language : ")
+d.update({name:lang})
+
+name = input ("enter your name : ")
+lang = input("enter your favourate language : ")
+d.update({name:lang})
+
+print(d) 
