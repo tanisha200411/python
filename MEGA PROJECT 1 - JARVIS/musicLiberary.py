2ae7def32c0d07ba4e2cf16b155216be07d0de4c
tanisha200411 <tanishakaranjiya@gmail.com>
2025-04-14 23:08:36 +0530

    Create musicLiberary.py

---
+++ MEGA PROJECT 1 - JARVIS/musicLiberary.py
@@ -0,0 +1,10 @@
+music = {
+    "naruto" : "https://www.youtube.com/watch?v=0qP9jkdRsYU",
+    "sizume" : "https://www.youtube.com/watch?v=qal34e9v_pk",
+    "choo lo": "https://www.youtube.com/watch?v=opwZ_PJ-F_E",
+    "dil mere":"https://www.youtube.com/watch?v=qLCLvzTGFVM",
+    "alone"   :"https://www.youtube.com/watch?v=ALZHF5UqnU4",
+    " Liggi" :"https://www.youtube.com/watch?v=6BYIKEH0RCQ&list=RDEMdmudmUSJSMh2lXsGLsUiIA&index=1",
+    "barso"  :"https://www.youtube.com/watch?v=JY6BCXm6p-4&list=RDEMdmudmUSJSMh2lXsGLsUiIA&index=27",
+    "udd gaye":"https://www.youtube.com/watch?v=v2-9rIL_f4w&list=RDEMdmudmUSJSMh2lXsGLsUiIA&index=3"
+}
